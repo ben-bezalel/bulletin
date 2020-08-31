894af0eb3ea36a1d2f819471c4007b63b16429e6
ben-bezalel <65789628+ben-bezalel@users.noreply.github.com>
2020-08-31 15:11:13 +0100

    added email style as well, and a third column. need to allow for seperate divisions in email and print next. rough draft to share with Simon.

--- bulletin.py
+++ bulletin.py
@@ -1,42 +1,65 @@
 import jinja2
 import markdown2
 import pdfkit
-import io
-#this script should take formatted markdown and insert it into the correct parts of the template. 
 
 #look at including images. 
 
 first = ""
 second = ""
-ref = 0
+ref = [0]
+sections = []
 
 ##with open('testbulletin.md', 'r') as f:
 lines = open('testbulletin.md', 'r').readlines()
-if "## Thought for the week" in lines:
+if "COLUMNBREAK\n" in lines:
+    i=0
     while i < len(lines):
-        if s == "## Thought for the week":
-            ref = i-1
-            break
-first = lines[0:ref]
-second = lines[ref:-1]
-        
-firsthtml = markdown2.markdown("\n".join(first))
-secondhtml = markdown2.markdown("\n".join(second))
+        if lines[i] == "COLUMNBREAK\n":
+            ref.append(i)
+        i += 1
+    j=0
+    ref.append(len(lines))
+    while j < len(ref)-1:
+        if j>0:
+            sections.append(lines[ref[j]+1:ref[j+1]])
+        else:
+            sections.append(lines[ref[j]:ref[j+1]])
+        j +=1
+    k=0
+    while k < len(sections): 
+        sections[k] = markdown2.markdown("\n".join(sections[k]))
+        k +=1
+
+#have breaks for email and breaks for print
 
 loader = jinja2.FileSystemLoader(searchpath="templates")
 env = jinja2.Environment(loader=loader)
 
+#I could probably remove jinja and replace with an ordered list...
 printtemplate = env.get_template("print.html.jinja")
-
+emailtemplate = env.get_template("email.html.jinja")
 printhtml = open ("print.html","w")
 
-printhtml.write(printtemplate.render(first=firsthtml, second=secondhtml))
+printhtml.write(printtemplate.render(first=sections[0], second=sections[2], third=sections[1]))
 
 printhtml.close()
 
+emailhtml = open ("email.html","w")
+
+emailhtml.write(emailtemplate.render(first=sections[0], second=sections[2], third=sections[1]))
+
+emailhtml.close()
+
 options = {
         'page-size':'A4',
-        'orientation':'Landscape'
+        'orientation':'Landscape',
+        'enable-local-file-access': None # so that external css files can be used to style
         }
 
 pdfkit.from_file("print.html", "print.pdf", options=options)
+
+#NOTES
+#deactive a pipenv environment by typing deactivate
+#wkhtmltopdf.org needs to be installed for script to work (or you can save to pdf from html)
+#start up pipenv by running pipenv shell
+#za to toggle folds in vim, ctrl-shift to move between cmd tabs, :b to move between vim buffers
